daf9fa0252eda83652a9014be1b8989d958c4a2d
Alex Bepple <alex@bepple.de>
2022-01-06 16:59:53 +0100

    $ move focused window to neighboring spaces

---
+++ phoenix.js
@@ -0,0 +1,27 @@
+const getActiveSpaceIdx = () => Space.all().findIndex(x => x.isEqual(Space.active()))
+const mod = (a, n) => (a+n) % n
+
+const moveFocusedWindowFromSpaceIdxToSpaceIdx = (fromIdx, unboundedToIdx) => {
+  const toIdx = mod(unboundedToIdx, Space.all().length)
+  Phoenix.log(`Moving focused window ${fromIdx} → ${toIdx}`)
+
+  const currentWindow = Window.focused()
+  Space.all()[toIdx].addWindows([currentWindow])
+  Space.all()[fromIdx].removeWindows([currentWindow])
+  currentWindow.focus()
+}
+
+/*
+> you must keep a reference to the handler, 
+> otherwise your callback will not get called.
+https://github.com/kasper/phoenix/blob/master/docs/API.md#getting-started
+*/
+const moveLeftKey = new Key('left', [ 'cmd', 'option', 'shift' ], () => {
+  const spaceIdx = getActiveSpaceIdx()
+  moveFocusedWindowFromSpaceIdxToSpaceIdx(spaceIdx, spaceIdx-1)
+})
+
+const moveRightKey = new Key('right', [ 'cmd', 'option', 'shift' ], () => {
+  const spaceIdx = getActiveSpaceIdx()
+  moveFocusedWindowFromSpaceIdxToSpaceIdx(spaceIdx, spaceIdx+1)
+})
